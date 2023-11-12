eb70be7592041d2282c996e819235f3bc0af3f87
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-11-12 14:38:56 +0530

    Load icons in navbars

--- src/modules/Header.js
+++ src/modules/Header.js
@@ -1,3 +1,7 @@
+import { TiThMenu } from "react-icons/ti";
+import { IoMdNotifications } from "react-icons/io";
+import pfp from "../assets/noavatar.png";
+
 <script
   src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js"
   crossorigin
@@ -5,10 +9,17 @@
 const Header = ({ toggleSidebar }) => {
   return (
     <div className="head-nav">
-      <div onClick={toggleSidebar} className="side-menu">
-        side
+      <div className="head-left">
+        <div onClick={toggleSidebar} className="side-menu">
+          <TiThMenu />
+        </div>
+        <div className="side-menu">
+          <IoMdNotifications />
+        </div>
       </div>
-      <div>Header</div>
+      <div>
+        <img className="head-pfp" src={pfp} alt="profile" />
+      </div>
     </div>
   );
 };
--- src/modules/Sidebar.js
+++ src/modules/Sidebar.js
@@ -1,7 +1,19 @@
+import { BiSolidBowlRice, BiBookContent, BiSolidHome } from "react-icons/bi";
+
 const Sidebar = ({ showSidebar }) => {
   return (
     <div className={showSidebar ? "side-nav show-sidebar" : "side-nav"}>
-      Sidebar
+      <ul>
+        <li>
+          <BiSolidHome /> Home
+        </li>
+        <li>
+          <BiBookContent /> Menu
+        </li>
+        <li>
+          <BiSolidBowlRice /> Mess
+        </li>
+      </ul>
     </div>
   );
 };
