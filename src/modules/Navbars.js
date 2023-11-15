a3db15a0277d2441cdde23d96db3714c955ab4e0
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-11-15 14:51:53 +0530

    Modify Navbar

--- src/modules/Navbars.js
+++ src/modules/Navbars.js
@@ -1,18 +1,36 @@
-import React, { useState } from "react";
-import Header from "./Header";
-import Sidebar from "./Sidebar";
+import { TiThMenu } from "react-icons/ti";
+import { BiSolidBowlRice, BiBookContent, BiSolidHome } from "react-icons/bi";
+
+import { IoMdNotifications } from "react-icons/io";
+import pfp from "../assets/noavatar.png";
+
 import "./navbars.css";
 
+<script
+  src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js"
+  crossorigin
+></script>;
 const Navbars = () => {
-  const [showSidebar, setShowSidebar] = useState(true);
-  const toggleSidebar = () => {
-    setShowSidebar((s) => !s);
-  };
   return (
-    <>
-      <Header toggleSidebar={toggleSidebar} />
-      <Sidebar showSidebar={showSidebar} />
-    </>
+    <div className="head-nav">
+      <div className="head-left">
+        <div>Hey there,</div>
+        <div>Sumu Divakaran!</div>
+      </div>
+      <div className="head-right">
+        <div className="icons">
+          <div className="head-btn">Home</div>
+          <div className="head-btn">Menu</div>
+          <div className="head-btn">Mess</div>
+        </div>
+        <div>
+          <span>
+            <IoMdNotifications />
+          </span>
+          <img className="head-pfp" src={pfp} alt="profile" />
+        </div>
+      </div>
+    </div>
   );
 };
 
