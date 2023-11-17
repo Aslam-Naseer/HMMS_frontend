ab33fe10c43f9649bb9674abcc78e56d7b5b1a6a
Ananthu Jayakumar <115489088+ananthu666@users.noreply.github.com>
2023-11-17 14:59:16 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
-
-import './App.css';
-import Login from './modules/Login';
-import Register from './modules/Reg'; 
-import Dashboard from './modules/Dashboard'; 
-import Mess1 from './modules/mess/Mess1'
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from 'react-router-dom';
+import "./App.css";
+import Login from "./modules/Login";
+import Register from "./modules/Reg";
+import Dashboard from "./modules/Dashboard";
+import Mess1 from "./modules/mess/Mess1";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbars from "./modules/Navbars";
-
-
-
-
-
 
 function App() {
   return (
     <Router>
-      <Navbars/>
+      <Navbars />
       <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/reg" element={<Register/>} />
-        <Route path="/home" element={<Dashboard/>}/>
-        <Route path="/home/mess" element={<Mess1/>}/>
+        <Route path="/" element={<Login />} />
+        <Route path="/reg" element={<Register />} />
+        <Route path="/home" element={<Dashboard />} />
+        <Route path="/home/mess" element={<Mess1 />} />
       </Routes>
-
     </Router>
   );
 }
