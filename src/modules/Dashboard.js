1126c266dad0bd9040c01fad57572d8f22dd30ef
Ananthu Jayakumar <115489088+ananthu666@users.noreply.github.com>
2023-11-18 10:09:18 +0530

    Dashboard userdet

--- src/modules/Dashboard.js
+++ src/modules/Dashboard.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./dashboard.css";
-import { useState } from "react";
-
-import "bootstrap/dist/css/bootstrap.min.css";
+import { useState ,useEffect} from "react";
 import DashTile from "./DashTile";
+
 export default function Dashboard() {
-  const [list, setList] = useState({});
-  const display = () => {
-    fetch("http://127.0.0.1:5000/list", {
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    
+  const getListBy = () => {
+    fetch("http://127.0.0.1:5000/dash", {
       method: "Post",
       headers: {
         "Content-Type": "application/json",
@@ -17,34 +18,42 @@ export default function Dashboard() {
       }),
     }).then((response) => {
       response.json().then((data) => {
-        console.log(data);
+        // console.log(data);
         setList(data);
+        
+        sessionStorage.setItem("username",data[0][10]);
+        
       });
     });
   };
+  getListBy();
+  }, []);
   return (
     <div className="dashboard">
       <div className="dash-div">
         College Details
-        <DashTile title={"KTU ID"} value={"TVE21CS000"} />
-        <DashTile title={"Branch"} value={"CSE"} />
-        <DashTile title={"Semester"} value={"S5"} />
+        
+        <DashTile title={"KTU ID"} value={list[0] ? list[0][0] : 'Loading...'} />
+
+        <DashTile title={"Branch"} value={list[0] ? list[0][2] : 'Loading...'} />
+        <DashTile title={"Semester"} value={list[0] ? list[0][3] : 'Loading...'} />
         <DashTile empty />
       </div>
 
       <div className="dash-div">
         Personal Details
-        <DashTile title={"email"} value={"examplemail@gmail.com"} />
-        <DashTile title={"phone number"} value={9090909090} />
-        <DashTile title={"gender"} value={"Male"} />
-        <DashTile title={"DOB"} value={"01/01/2002"} />
+        <DashTile title={"email"} value={list[0] ? list[0][4] : 'Loading...'} />
+        <DashTile title={"phone number"} value={list[0] ? list[0][5] : 'Loading...'} />
+        <DashTile title={"gender"} value={list[0] ? list[0][6] : 'Loading...'} />
+        <DashTile title={"DOB"} value={list[0] ? new Date(list[0][7]).toLocaleDateString() : 'Loading...'} />
+
       </div>
 
       <div className="dash-div">
         Hostel Details
-        <DashTile title={"Hostel ID"} value={"MH-9999"} />
-        <DashTile title={"Block Number"} value={1} />
-        <DashTile title={"Room Number"} value={101} />
+        <DashTile title={"Hostel ID"} value={list[0] ? list[0][1] : 'Loading...'} />
+        <DashTile title={"Block Number"} value={list[0] ? list[0][8] : 'Loading...'}/>
+        <DashTile title={"Room Number"} value={list[0] ? list[0][9] : 'Loading...'} />
         <DashTile empty />
       </div>
     </div>
